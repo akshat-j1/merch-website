02633fdb23f7355d7d49f6156431289f5aea7963
akshat-j1 <224862220+akshat-j1@users.noreply.github.com>
2025-09-15 18:31:27 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 // js/main.js
 
-import { updateCartCount, renderCart, attachAddToCartListeners } from './cart.js';
+import { updateCartCount, renderCart, attachAddToCartListeners, attachClearCartListener } from './cart.js';
 import { initAuthUI, initCustomizeForm } from './firebase-form.js';
 
 // Run functions when the DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
+    // Cart-related functions
     updateCartCount();
     renderCart();
     attachAddToCartListeners();
+    attachClearCartListener();
+
+    // Firebase-related functions
     initAuthUI();
-    initCustomizeForm();
+    // Only call initCustomizeForm if the form element exists on the page
+    if (document.getElementById('customize-form')) {
+      initCustomizeForm();
+    }
 });
